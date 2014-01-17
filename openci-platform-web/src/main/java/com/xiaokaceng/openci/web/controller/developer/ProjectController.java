ce6d68b07a3e2dbffff89e2da2791bd6d9659b28
xinmin.fang@gmail.com <xinmin.fang@gmail.com>
2014-01-17 14:31:45 +0800

    project controller获取功能依赖和模块依赖

--- openci-platform-web/src/main/java/com/xiaokaceng/openci/web/controller/developer/ProjectController.java
+++ openci-platform-web/src/main/java/com/xiaokaceng/openci/web/controller/developer/ProjectController.java
@@ -1,22 +1,21 @@
 package com.xiaokaceng.openci.web.controller.developer;
 
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.openkoala.koala.queryvo.TypeDef;
+import org.openkoala.koala.util.ModuleDependencyUtils;
+import org.openkoala.koala.widget.Module;
+import org.openkoala.koala.widget.Project;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.dayatang.querychannel.support.Page;
-import com.xiaokaceng.openci.application.DeveloperApplication;
 import com.xiaokaceng.openci.application.ProjectApplication;
 import com.xiaokaceng.openci.application.dto.ProjectDto;
-import com.xiaokaceng.openci.domain.Developer;
 import com.xiaokaceng.openci.web.controller.BaseController;
 import com.xiaokaceng.openci.web.dto.ResultDto;
 
@@ -34,6 +33,27 @@ public class ProjectController extends BaseController {
 		return ResultDto.createSuccess();
 	}
 	
+	@ResponseBody
+    @RequestMapping("/get-project-instance")
+	public ProjectDto getProjectInstance() {
+		return new ProjectDto();
+	}
+	
+	@ResponseBody
+    @RequestMapping("/get-functions")
+	public Map<String, Map<String, String>> getFunctions(String moduleType) {
+		Map<String, Map<String, String>> result = new HashMap<String, Map<String,String>>();
+		TypeDef typeDef = TypeDef.getInstance();
+		result.put("functions", typeDef.getFunctions(moduleType));
+		return result;
+	}
+	
+	@ResponseBody
+    @RequestMapping("/get-dependables")
+	public List<Module> getDependables(Project project, String moduleType) {
+		ModuleDependencyUtils moduleDependencyUtils = new ModuleDependencyUtils(project, moduleType);
+		return moduleDependencyUtils.getCouldDependencyModules();
+	}
 	
 	
 //	@ResponseBody
