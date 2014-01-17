bae976878f435b9365c233f5ca5cc851cdb271ac
xiaokaceng <luomin@foreveross.com>
2014-01-17 10:41:13 +0800

    工具配置实现

--- openci-platform-web/src/main/java/com/xiaokaceng/openci/web/controller/toolconfiguration/ToolConfigurationController.java
+++ openci-platform-web/src/main/java/com/xiaokaceng/openci/web/controller/toolconfiguration/ToolConfigurationController.java
@@ -1,5 +1,9 @@
 package com.xiaokaceng.openci.web.controller.toolconfiguration;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
@@ -7,8 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.dayatang.querychannel.support.Page;
 import com.xiaokaceng.openci.application.ToolConfigurationApplication;
-import com.xiaokaceng.openci.domain.Developer;
 import com.xiaokaceng.openci.domain.ToolConfiguration;
 import com.xiaokaceng.openci.web.controller.BaseController;
 import com.xiaokaceng.openci.web.dto.ResultDto;
@@ -45,4 +49,33 @@ public class ToolConfigurationController extends BaseController {
 		return ResultDto.createSuccess();
 	}
 	
+	@ResponseBody
+    @RequestMapping("/unusable/{toolConfigurationId}")
+	public ResultDto setToolUnUsable(@PathVariable long toolConfigurationId) {
+		ToolConfiguration toolConfiguration = ToolConfiguration.get(ToolConfiguration.class, toolConfigurationId);
+		if (toolConfiguration == null) {
+			return ResultDto.createFailure();
+		}
+		toolConfigurationApplication.setToolUnusabled(toolConfiguration);
+		return ResultDto.createSuccess();
+	}
+	
+	@ResponseBody
+    @RequestMapping("/get-all-usable")
+	public List<ToolConfiguration> getAllUsable() {
+		return toolConfigurationApplication.getAllUsable();
+	}
+	
+	@ResponseBody
+    @RequestMapping("/pagingquery")
+	public Map<String, Object> pagingQuery(int page, int pagesize) {
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		Page<ToolConfiguration> toolConfigurationPage  = toolConfigurationApplication.pagingQeuryToolConfigurations(page, pagesize);
+		dataMap.put("Rows", toolConfigurationPage.getResult());
+		dataMap.put("start", page * pagesize - pagesize);
+		dataMap.put("limit", pagesize);
+		dataMap.put("Total", toolConfigurationPage.getTotalCount());
+		return dataMap;
+	}
+
 }
