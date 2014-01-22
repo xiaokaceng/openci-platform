40e696685e95a413b26bcb23be9e1026a97eb97f
xiaokaceng <luomin@foreveross.com>
2014-01-22 18:46:28 +0800

    添加KoalaScmConfig方法

--- openci-platform-applicationImpl/src/main/java/com/xiaokaceng/openci/pojo/JenkinsConfigurationPojo.java
+++ openci-platform-applicationImpl/src/main/java/com/xiaokaceng/openci/pojo/JenkinsConfigurationPojo.java
@@ -1,8 +1,12 @@
 package com.xiaokaceng.openci.pojo;
 
 import org.openkoala.opencis.jenkins.JenkinsCISClient;
+import org.openkoala.opencis.jenkins.KoalaGitConfig;
+import org.openkoala.opencis.jenkins.KoalaScmConfig;
+import org.openkoala.opencis.jenkins.KoalaSvnConfig;
 
 import com.xiaokaceng.openci.application.dto.ScmConfig;
+import com.xiaokaceng.openci.application.dto.ScmType;
 import com.xiaokaceng.openci.domain.JenkinsConfiguration;
 import com.xiaokaceng.openci.domain.ToolConfiguration;
 
@@ -18,10 +22,23 @@ public class JenkinsConfigurationPojo extends ToolConfigurationPojo {
 	public void createCISClient(ToolConfiguration toolConfiguration) {
 		if (toolConfiguration instanceof JenkinsConfiguration) {
 			JenkinsCISClient jenkinsCISClient = new JenkinsCISClient(toolConfiguration.getServiceUrl(), toolConfiguration.getUsername(), toolConfiguration.getPassword());
-			System.out.println(scmConfig.getScmType());
+			jenkinsCISClient.setKoalaScmConfig(createKoalaScmConfig());
 			cisClient = jenkinsCISClient;
 			isInstance = true;
 		}
+	}
+
+	private KoalaScmConfig createKoalaScmConfig() {
+		if (scmConfig == null) {
+			return null;
+		}
+		if (scmConfig.getScmType().equals(ScmType.SVN)) {
+			return new KoalaSvnConfig(scmConfig.getRepositoryUrl());
+		}
+		if (scmConfig.getScmType().equals(ScmType.GIT)) {
+			return new KoalaGitConfig(scmConfig.getRepositoryUrl());
+		}
+		return null;
 	}
 
 }
