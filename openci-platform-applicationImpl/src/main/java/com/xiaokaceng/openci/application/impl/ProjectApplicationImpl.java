c6a02fd213622a72c3defcbea2c7f166edf8bbb6
xiaokaceng <luomin@foreveross.com>
2014-02-19 17:37:24 +0800

    项目整合状态更新

--- openci-platform-applicationImpl/src/main/java/com/xiaokaceng/openci/application/impl/ProjectApplicationImpl.java
+++ openci-platform-applicationImpl/src/main/java/com/xiaokaceng/openci/application/impl/ProjectApplicationImpl.java
@@ -125,6 +125,7 @@ public class ProjectApplicationImpl implements ProjectApplication {
 	private void updateProjectStatus(Tool tool) {
 		Project project = tool.getProject();
 		project.updateProjectStatus();
+		
 	}
 
 }
